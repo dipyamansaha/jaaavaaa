8bef996f1a09f48d7fc4c1ca99dd95b8f58f2331
sahooashutosh <72198950+sahooashutosh@users.noreply.github.com>
2020-10-01 16:52:47 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -3,3 +3,13 @@ public static void main(String args[]){
 Scannerr sc = new Scanner (System.in){
 int a = sc.nextInt();
 int b = sc.nextInt();
+  if(a %2 == 0){
+    System.out.println("even");
+  }else{
+    System.out.println("odd");
+    if(b %2 == 0){
+    System.out.println("even");
+  }else{
+    System.out.println("odd");
+    
+  
